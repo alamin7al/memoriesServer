5429084421c8c1d05126e3102ba4c76ca658d1f2
alamin <ma7029582@gmail.com>
2022-04-18 22:37:04 +0600

    update

--- index.js
+++ index.js
@@ -23,12 +23,15 @@ async function run() {
     try {
         await client.connect()
         const database = client.db('data')
+        //assignment code
         const usdCollection = database.collection('usd')
         const GBRCollection = database.collection('gbr')
         const EurCollection = database.collection('eur')
+        //assignment code
+        const userDataCollection = database.collection('user')
 
 
-
+        //assignment code
         app.get('/usd', async (req, res) => {
             const cursor = usdCollection.find({})
             const user = await cursor.toArray()
@@ -48,11 +51,77 @@ async function run() {
             res.send(user)
         })
 
+        //assignment code
 
 
-           
 
-        
+
+
+
+
+
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            // console.log('hit the post api', service); 
+
+            const result = await userDataCollection.insertOne(user);
+            res.send(result)
+        });
+
+        app.get('/useremail', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const cursor = userDataCollection.find(query)
+            const user = await cursor.toArray()
+            res.json(user)
+        })
+
+        app.put('/useremail', async (req, res) => {
+            const updateUser = req.body
+
+            const options = { upsert: true }
+
+            const updateDoc = {
+                $set: {
+                    address: updateUser.address,
+                    bio: updateUser.bio,
+                    date: updateUser.date,
+                    displayName: updateUser.displayName,
+                    email: updateUser.email,
+                    facebook: updateUser.facebook,
+                    gender: updateUser.gender,
+                    insta: updateUser.insta,
+                    profession: updateUser.profession,
+                    linke: updateUser.linke,
+                    website: updateUser.website,
+
+                }
+            }
+            const result = await userDataCollection.updateOne(updateDoc, options)
+            console.log(result);
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         console.log('j');
